f785ed6a5fe365b80d2e9d06251c6e5e685c7078
Admin <376565795@qq.com>
2020-07-22 11:44:05 +0800

    update

---
+++ dist/precache-manifest.97fcc4d31e2d46a190da9a7992865910.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a03f92a797e309e76e6a034ab9e02616",
+    "url": "../dist/data/haarcascade_frontalface_default.xml"
+  },
+  {
+    "revision": "d203805a37d279b482376f6d459ede64",
+    "url": "../dist/index.html"
+  },
+  {
+    "revision": "2bd355badfa545a54b383cbd1ed8f463",
+    "url": "../dist/libs/opencv.js"
+  },
+  {
+    "revision": "a88bd342be2995031eba74087c572947",
+    "url": "../dist/libs/opencv_js.js"
+  },
+  {
+    "revision": "7c411a712ee97c5f26b92b1ed0fc8d7a",
+    "url": "../dist/libs/opencv_js.wasm"
+  },
+  {
+    "revision": "62792e832fcb22d445f43797e4b58920",
+    "url": "../dist/libs/templateMatch.cpp"
+  },
+  {
+    "revision": "0921b18abc39f1904a318b697f3ccea0",
+    "url": "../dist/libs/templateMatch.js"
+  },
+  {
+    "revision": "2123beab7abba1ac9c5b7b5ad32bfb2f",
+    "url": "../dist/libs/templateMatch.wasm"
+  },
+  {
+    "revision": "c1879101f5822acfa2e7af01c7aa30d3",
+    "url": "../dist/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "../dist/robots.txt"
+  },
+  {
+    "revision": "44b4de794fb88dd0a314",
+    "url": "../dist/static/css/app.0b1fccbd.css"
+  },
+  {
+    "revision": "084308e21f5a68b4fb63",
+    "url": "../dist/static/js/about.347e7793.js"
+  },
+  {
+    "revision": "44b4de794fb88dd0a314",
+    "url": "../dist/static/js/app.4edc9531.js"
+  },
+  {
+    "revision": "a94020ad75abadec6cf0",
+    "url": "../dist/static/js/chunk-vendors.fa72d815.js"
+  }
+]);
