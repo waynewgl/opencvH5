c939b36bcf342c0d6cba2fa9f31aab0664b91052
Admin <376565795@qq.com>
2020-07-22 18:32:55 +0800

    update

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.b9cdb7f8461f841835207d8ad04a247b.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.765174accbfdb1f1695e4ac54c53a074.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 if (workbox) {
 // 缓存web的css资源
